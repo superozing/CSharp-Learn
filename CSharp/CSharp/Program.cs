99be00554bb284e3ad1b041afee1d2986fa9a3cd
superozing <amazingozing@gmail.com>
2024-09-09 21:08:12 +0900

    섹션2/ 데이터 가지고 놀기

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -2,8 +2,13 @@
 {
     class Program
     {
+        // 주석.
         static void Main(string[] args)
         {
+            // int - 정수형
+            // float - 실수형
+            // string - 문자열 (이런게 기본 자료형이라고?)
+            // bool - 참/거짓
             Console.WriteLine("Hello, World!");
         }
     }
