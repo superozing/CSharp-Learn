ae4231b2242ad66467ef3431f83e94593df44735
superozing <amazingozing@gmail.com>
2024-09-21 17:12:16 +0900

    섹션 3/ 오버로딩, 열거형

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,6 +4,19 @@
     {
         // 주석.
 
+        // C# 이넘은 C++ 이넘 클래스와 같은 느낌이라고 보면 된다. 안전하게 명시적으로 형변환을 해주어야 한다.
+        // 기본적으로 int 이지만, 다른 타입으로도 저장할 수 있도록 설계 되어 있음(정수형만 해당함).
+        enum Days : byte
+        {
+            Sunday = 1,
+            Monday,    // 2
+            Tuesday,   // 3
+            Wednesday, // 4
+            Thursday = 10,
+            Friday,    // 11
+            Saturday   // 12
+        }
+
         static void initOut(out int a)
         {
             // a의 레퍼런스를 받아오는 것은 ref와 같다.
@@ -24,6 +37,19 @@
             // a는 복사본을 가져옴.
             a++;
         }
+
+        // 오버로딩 - 같은 이름의 함수를 여러 개 정의하는 것.
+        // 오버로딩의 조건:
+        // 정확하게 동일한 인자 개수와 타입을 가지면 안된다.
+        static void Overload(int a, int b) { }
+        static void Overload(int a, float b) { }
+        
+        // 선택적 매개변수(기본 인자)
+        // 선택적 매개변수 자리에 아무 값을 입력하지 않는다면, 기본으로 설정된 값을 사용하겠다. 라는 의미입니당
+        // C++ 기본 인자와 같은 기능읗 하네요.
+        static void Overload(int a, int b, int c, int d = 0, float e = 0.0f) { }
+
+
 
         static void Main(string[] args)
         {
@@ -107,6 +133,12 @@
             // out은 보통 메서드 안에서 값을 할당해야만 하는 상황에 쓰임(여러 개의 반환이 필요하다거나..)
             // out 키워드를 명시적으로 붙여주어야 함.
             initOut(out iRef);
+
+            // 오버로딩
+            Overload(1, 2, 3, e: 1.0f);
+
         }
+
+
     }
 }
